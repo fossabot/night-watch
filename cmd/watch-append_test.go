0b6dcf44443e0fbdeeae23df4e92a10c436d7998
montavista <44042823+montavista@users.noreply.github.com>
2018-12-26 01:49:12 -0800

    add status file checking and file size change test (#12)
    * add status file checking test
    
    * package change
    
    * test file content append
    
    * add tests in ci
    
    * enable flag

---
+++ cmd/watch-append_test.go
@@ -0,0 +1,102 @@
+package cmd
+
+import (
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"night-watch/watch-append"
+	"os"
+	"strings"
+	"testing"
+)
+
+var testpath = "/tmp/dashbase-test"
+
+func setupTestCase(t *testing.T) {
+	t.Log("setup test case")
+	if _, err := os.Stat(testpath); err == nil {
+		os.RemoveAll(testpath)
+	}
+	if _, err := os.Stat(testpath); os.IsNotExist(err) {
+		os.Mkdir(testpath, 0744)
+	}
+}
+
+func TestStatusInfo(t *testing.T) {
+	setupTestCase(t)
+	f1 := []byte("test\ndashbase\n")
+	//length := len(f1)
+	err := ioutil.WriteFile(testpath+string(os.PathSeparator)+"dat1.log", f1, 0644)
+	checkError(t, err)
+	f2 := []byte("test\ndashbase\n")
+	err = ioutil.WriteFile(testpath+string(os.PathSeparator)+"dat2.log", f2, 0644)
+	checkError(t, err)
+	f3 := []byte("test\ndashbase\n")
+	err = ioutil.WriteFile(testpath+string(os.PathSeparator)+"_dat3.log", f3, 0644)
+	checkError(t, err)
+
+	state := watch_append.NewStates()
+	metric := watch_append.NewWatchMetric()
+	metric.Start()
+
+	state.Scan(testpath+string(os.PathSeparator)+"*.log", []string{"_"}, &metric)
+	statusfile := testpath + string(os.PathSeparator) + "status.txt"
+	state.Save(statusfile)
+	dat, err := ioutil.ReadFile(statusfile)
+	checkError(t, err)
+
+	result := string(dat)
+	if len(result) == 0 {
+		assert.Fail(t, "empty state file")
+	}
+	osf, err := watch_append.LoadStates(statusfile)
+
+	if len(osf.States) <= 0 {
+		assert.Fail(t, "wrong state file")
+	}
+
+	checkData(t, osf)
+}
+
+func TestAppend(t *testing.T) {
+	osf, err := watch_append.LoadStates(testpath + string(os.PathSeparator) + "status.txt")
+	if err != nil {
+		checkError(t, err)
+	}
+	metric := watch_append.NewWatchMetric()
+	metric.Start()
+
+	f, err := os.OpenFile(testpath+string(os.PathSeparator)+"dat1.log", os.O_APPEND|os.O_WRONLY, 0600)
+	checkError(t, err)
+	defer f.Close()
+	if _, err = f.WriteString("add a line\n"); err != nil {
+		checkError(t, err)
+	}
+
+	asf := watch_append.NewStates()
+	asf.Scan(testpath+string(os.PathSeparator)+"*.log", []string{"_"}, &metric)
+
+	diff := watch_append.NewDiff(asf, osf, pattern, &metric)
+	diff.Diff()
+	diff.Result.TotalSize += osf.TotalSize
+	asf.SetTotalSize(diff.Result.TotalSize)
+	asf.Save(testpath + string(os.PathSeparator) + "status.txt")
+	assert.Equal(t, int64(11), diff.Result.TotalSize)
+	assert.Equal(t, int64(2), diff.Result.Count)
+}
+
+func checkData(t *testing.T, osf watch_append.States) {
+	for _, v := range osf.States {
+		if (strings.Contains(v.Source, "dat1") || strings.Contains(v.Source, "dat2")) && !strings.Contains(v.Source, "dat3") {
+			t.Log(v.Source)
+			assert.True(t, true, "string contains dat1 or dat2 and not dat3")
+		} else {
+			assert.Fail(t, "missing log")
+		}
+	}
+}
+
+func checkError(t *testing.T, e error) {
+	if e != nil {
+		assert.Fail(t, e.Error())
+	}
+}
--- go.mod
+++ go.mod
@@ -1,14 +1,12 @@
 module night-watch
 
 require (
-	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
-	github.com/davecgh/go-spew v1.1.1
-	github.com/google/pprof v0.0.0-20181206194817-3ea8567a2e57 // indirect
-	github.com/ianlancetaylor/demangle v0.0.0-20181102032728-5e5cf60278f6 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
-	github.com/pkg/profile v1.2.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/cobra v0.0.3
 	github.com/spf13/pflag v1.0.3 // indirect
+	github.com/stretchr/testify v1.2.2
 	go.uber.org/atomic v1.3.2 // indirect
 	go.uber.org/multierr v1.1.0 // indirect
 	go.uber.org/zap v1.9.1
